0c0a60f4f22c4d9adcd28c341f75334840cc727c
Karen Zapata <peraltakaren662@gmail.com>
2025-10-26 15:09:42 -0500

    Se crea clase base de profesor y Departamento

--- models.py
+++ models.py
@@ -18,3 +18,14 @@ class CursoBase(SQLModel):
     nombre: str = Field(description="Nombre curso")
     creditos: int= Field(description="Creditos curso")
     horario: str = Field(description= "Horario curso (ej: Lunes 8-10)")
+
+class ProfesorBase(SQLModel):
+    cedula: str = Field(unique=True, index=True, description="Cedula unica profesor")
+    nombre: str = Field(description="Nombre profesor")
+    email: str = Field(description="Email profesor")
+    titulo : str = Field(description="Titulo profesor (ej: PhD, MSc, Ingeniero)")
+    activo: bool = Field(default=True, description="Estado del profesor")
+
+class DepartamentoBase(SQLModel):
+    codigo: str = Field(unique=True, index= True ,description="Codigo departamento (ej: ING, HUM)")
+    nombre: str = Field(description="Nombre departamento")
