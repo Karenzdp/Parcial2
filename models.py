b9e4607332bf1de2d425b2c892c2af7c900b3975
Karen Zapata <peraltakaren662@gmail.com>
2025-10-26 14:41:18 -0500

    Se crea el modelo base Estudiante

--- models.py
+++ models.py
@@ -3,3 +3,10 @@ from typing import Optional
 from pydantic import field_validator, EmailStr
 
 # MODELOS BASE
+
+class EstudianteBase(SQLModel):
+    cedula: str = Field(unique=True, index=True, description="Cédula única estudiante")
+    nombre: str = Field(description="Nombre completo estudiante")
+    email: str = Field(unique=True, description="Email institucional estudiante")
+    semestre: str = Field(description="Semestre actual estudiante")
+    activo: bool = Field(default=True, description="Estado estudiante")
