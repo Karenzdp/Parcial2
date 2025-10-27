e251a8bf466ce8ba0add96aeb4d4e99a2a7f93fe
Karen Zapata <peraltakaren662@gmail.com>
2025-10-26 21:46:00 -0500

    Validar cédula y nombre en el endpoint de creación de estudiantes

--- estudiantes.py
+++ estudiantes.py
@@ -3,7 +3,24 @@ from sqlmodel import select
 from db import SessionDep
 from models import (
     Estudiante, EstudianteCreate, EstudianteUpdate,
-    EstudianteConCursos, Curso
+    EstudianteconCursos, Curso
 )
 import re
+
+router=APIRouter()
+
+@router.post("/", response_model=Estudiante, status_code=201, summary="Crear estudiante")
+async def crear_estudiante(nuevo_estudiante:EstudianteCreate, session: SessionDep):
+
+    if not nuevo_estudiante.cedula.isdigit():
+        raise HTTPException(status_code=400, detail="La cedula solo puede contener números")
+
+    if len(nuevo_estudiante.cedula) < 5 or len(nuevo_estudiante.cedula) > 12:
+        raise HTTPException(status_code=400, detail="La cédula debe tener entre 5 y 12 dígitos")
+
+    if not nuevo_estudiante.nombre.strip()
+        raise HTTPException(status_code=400, detail="El nombre no puede estar vacio")
+
+    if not all(c.isalpha() or c.isspace() for c in nuevo_estudiante.nombre):
+        raise HTTPException(status_code=400, detail="El nombre solo puede contener letras y espacios")
 
